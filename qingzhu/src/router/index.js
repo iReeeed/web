dfff7459f978aa413351ab2d1753bc5a23ca840f
iRed <Littlered111@163.com>
2020-10-26 20:16:39 +0800

    add:新增订单确认页面

--- qingzhu/src/router/index.js
+++ qingzhu/src/router/index.js
@@ -4,11 +4,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'Personal',
-    component: () =>
-        import ( /* webpackChunkName: "about" */ '../views/Personal.vue')
-}]
+        path: '/',
+        name: 'Personal',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Personal.vue')
+    },
+    {
+        path: '/checkout',
+        name: 'Checkout',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Checkout.vue')
+    }
+]
 
 const router = new VueRouter({
     routes
