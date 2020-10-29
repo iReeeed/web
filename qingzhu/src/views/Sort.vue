186796e7bee2ea6571e350d69d6faeff15fc3ca0
iRed <Littlered111@163.com>
2020-10-29 17:19:54 +0800

    fix:首页index

---
+++ qingzhu/src/views/Sort.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+      <div class="outer">
+        <div class="inter">
+            <a href=""class="logo"><img src="images/logo.png" alt=""></a>
+            <ul>
+                <li><a href="" id="cha">查看所有类别</a></li>
+                <li><a href="">首页</a></li>
+                <li><a href="">所有产品</a></li>
+                <li><a href="">博客</a></li>
+                <li><a href="">文章列表</a></li>
+            </ul>
+            <a href="" class="sousuo"><i class="icon iconfont icon-sousuo"></i></a>
+            <div class="inter_right">
+                <a href="./青竹登录.html">登录</a>
+                <span>|</span>
+                <a href="./青竹注册.html">注册</a>
+                <a href="./购物车.html"><i class="icon iconfont icon-gouwuche" id="shop"></i>0</a>
+            </div>
+            <div class="shop">
+                <p>3件商品 总计:¥1111</p>
+                <div class="shop1">
+                    <a href="购物车.html">去结算</a>
+                </div>
+            </div>
+            <div class="all">
+                <div class="all1">
+                    <a href="">不锈钢</a><br>
+                    <a href="">原料水泥</a><br>
+                    <a href="">塑料</a><br>
+                    <a href="">木制</a><br>
+                    <a href="">陶瓷</a>
+                </div>
+                <div class="all2">
+                    <a href=""><img src="images/png13.png" alt=""></a>
+                    <a href=""><img src="images/png14.png" alt=""></a>
+                    <a href=""><img src="images/s1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script src="@/assets/Home/js/jqurey.js"></script>
+<script src="@/assets/Home/js/查看所有类别.js"></script>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import '../assets/Home/css/青竹0.css';
+@import '../assets/Home/css/查看所有类别.css';
+
+</style>
